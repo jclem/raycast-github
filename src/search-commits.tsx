106fc1869e81430850f6a0c19cec17336bad774c
Jonathan Clem <jclem@github.com>
2021-11-04 14:52:21 -0400

    Add commit search

--- src/search-commits.tsx
+++ src/search-commits.tsx
@@ -5,34 +5,36 @@ import Search from './components/search'
 import icon from './lib/icon'
 import {octokit} from './lib/octokit'
 
-export type CodeResult =
-  RestEndpointMethodTypes['search']['code']['response']['data']['items'][number]
+export type Commit =
+  RestEndpointMethodTypes['search']['commits']['response']['data']['items'][number]
 
 interface ActionsProps {
-  item: CodeResult
+  item: Commit
 }
 
-export default function CodeSearch(): ReactElement {
+export default function CommitSearch(): ReactElement {
   return (
-    <Search<CodeResult>
-      queryKey={query => ['search', 'code', query]}
+    <Search<Commit>
+      queryKey={query => ['search', 'commits', query]}
       queryFn={async q => {
-        const resp = await octokit.search.code({q})
+        const resp = await octokit.search.commits({q})
         const repos = resp.data.items
         return repos
       }}
       itemProps={result => ({
-        key: result.html_url,
-        title: result.repository.full_name,
-        subtitle: result.path,
-        icon: result.repository.owner.avatar_url
+        key: result.node_id,
+        title: result.commit.message,
+        subtitle: result.author?.login,
+        icon: result.author?.avatar_url,
+        accessoryTitle: result.repository.full_name,
+        accessoryIcon: result.repository.owner.avatar_url
       })}
-      actions={CodeActions}
+      actions={CommitActions}
     />
   )
 }
 
-function CodeActions({item}: ActionsProps): ReactElement {
+function CommitActions({item}: ActionsProps): ReactElement {
   return (
     <ActionPanel>
       <OpenInBrowserAction
